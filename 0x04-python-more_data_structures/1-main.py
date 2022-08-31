287bf5297a6e6e9273efe82e5bda1e0ab99882cd
Surina-Singh <surinsingh@hotmail.co.za>
2022-08-31 08:06:39 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-main.py
+++ 0x04-python-more_data_structures/1-main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    copy = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            copy.append(replace)
-        else:
-            copy.append(my_list[i])
-    return copy
+search_replace = __import__('1-search_replace').search_replace
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
